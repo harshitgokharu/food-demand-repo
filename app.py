17e3fed2d3d5ae9ea02b399049c8c7360981021c
harshitgokharu <55587562+harshitgokharu@users.noreply.github.com>
2020-06-17 12:28:56 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -7,6 +7,17 @@ app = Flask(__name__)
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 model_jan = pickle.load(open('model_jan.pkl', 'rb'))
 model_feb = pickle.load(open('model_feb.pkl', 'rb'))
+model_mar = pickle.load(open('model_mar.pkl', 'rb'))
+model_apr = pickle.load(open('model_apr.pkl', 'rb'))
+model_may = pickle.load(open('model_may.pkl', 'rb'))
+model_jun = pickle.load(open('model_jun.pkl', 'rb'))
+model_jul = pickle.load(open('model_jul.pkl', 'rb'))
+model_aug = pickle.load(open('model_aug.pkl', 'rb'))
+model_sep = pickle.load(open('model_sep.pkl', 'rb'))
+model_oct = pickle.load(open('model_oct.pkl', 'rb'))
+model_nov = pickle.load(open('model_nov.pkl', 'rb'))
+model_dec = pickle.load(open('model_dec.pkl', 'rb'))
+
 
 @app.route('/')
 def home():
@@ -26,8 +37,26 @@ def predict():
        prediction = model_jan.predict(final_features)
     elif request.form.get("month") == "2":
        prediction = model_feb.predict(final_features)
-    
-    
+    elif request.form.get("month") == "3":
+       prediction = model_mar.predict(final_features)
+    elif request.form.get("month") == "4":
+       prediction = model_apr.predict(final_features)
+    elif request.form.get("month") == "5":
+       prediction = model_may.predict(final_features)
+    elif request.form.get("month") == "6":
+       prediction = model_jun.predict(final_features)
+    elif request.form.get("month") == "7":
+       prediction = model_jul.predict(final_features)
+    elif request.form.get("month") == "8":
+       prediction = model_aug.predict(final_features)
+    elif request.form.get("month") == "9":
+       prediction = model_sep.predict(final_features)
+    elif request.form.get("month") == "10":
+       prediction = model_oct.predict(final_features)
+    elif request.form.get("month") == "11":
+       prediction = model_nov.predict(final_features)
+    elif request.form.get("month") == "12":
+       prediction = model_dec.predict(final_features)
     
     
     count = 0
